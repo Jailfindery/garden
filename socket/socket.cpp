09cc6c5ccae640b48d9e4f59834a9b0b785a6bfb
Jailfindery <joshua.g.schell@gmail.com>
2013-02-13 23:36:48 -0600

    Added license information

--- socket/socket.cpp
+++ socket/socket.cpp
@@ -1,3 +1,23 @@
+/*
+ *
+ *  Socket implementation class functions for garden.
+ *  Copyright (C) 2013 Joshua Schell (joshua.g.schell@gmail.com)
+ *
+ *  garden is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  garden is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with garden.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <cstdlib>
 #include <cstring>
 #include <unistd.h>
@@ -6,8 +26,6 @@
 #include <netinet/in.h>
 
 #include "socket.h"
-
-#include <iostream>
 
 /* One may be able to override this,
  * but I have never tried...
--- socket/socket.h
+++ socket/socket.h
@@ -1,7 +1,27 @@
+/*
+ *
+ *  Socket implementation class header for garden.
+ *  Copyright (C) 2013 Joshua Schell (joshua.g.schell@gmail.com)
+ *
+ *  garden is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  garden is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with garden.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef SOCKET_H_INCLUDED
 #define SOCKET_H_INCLUDED
 
-#include <iostream>
+#include <string>
 #include <sys/types.h>
 #include <netinet/in.h>
 
@@ -36,4 +56,4 @@ namespace tcp
 }
 
 #endif
-		
+
